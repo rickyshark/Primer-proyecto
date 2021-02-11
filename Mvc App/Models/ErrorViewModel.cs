48e48b46bb32e5197a81f4fa88bdcc8f7c80f205
rickylasalsa <monteroricky4@gmail.com>
2021-02-11 17:17:00 -0400

    Creado proyecto MVC

---
+++ Mvc App/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Mvc_App.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
