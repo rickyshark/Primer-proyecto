77ffc97e39f13a87120d2423f5dbdab5aaea79cb
Rangel Acevedo <rangelfco123@gmail.com>
2021-02-24 00:07:19 -0400

    Se agrego la entidad ofertas de trabajo y controlador

--- ApiSitio/Contexts/AppDbContext.cs
+++ ApiSitio/Contexts/AppDbContext.cs
@@ -16,5 +16,6 @@ namespace ApiSitio.Contexts
         public DbSet<Usuario1> Usuario1 { get; set; }
         public DbSet<Correo> Correo { get; set; }
         public DbSet<Report_Offer> Report_Offer { get; set; }
+        public DbSet<Offer_Job> Offer_Job { get; set; }
     }
 }
---
+++ ApiSitio/Entities/Offer_Job.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ApiSitio.Entities
+{
+    public class Offer_Job
+    {
+        [Key]
+        public int Id { get; set; }
+        public int Id_Usuario { get; set; }
+        public int Id_Categoria { get; set; }
+        public string Tipo { get; set; }
+        public int Logo { get; set; }
+        public string Posicion { get; set; }
+        public string Ciudad { get; set; }
+        public string Descripcion { get; set; }
+        [DisplayFormat(DataFormatString = "{0:yyyy/dd/MM}", ApplyFormatInEditMode = true)]
+        public DateTime? Fecha { get; set; }
+    }
+}
